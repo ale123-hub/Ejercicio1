1bc557faad14ae78da27cf727e4423b0512666dc
ale123-hub <jonathanquito2001@hotmail.com>
2021-06-16 16:17:09 -0500

    Ejercicio1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiPrimeraApp.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+        }
+    }
+}
